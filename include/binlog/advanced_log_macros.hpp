81a89f7c392b52b865ed2a7cb5752e74572bc4ac
Benedek Thaler <Benedek.Thaler@morganstanley.com>
2019-11-19 12:38:27 +0100

    Add BINLOG_<SEVERITY>_WC and BINLOG_<SEVERITY>_W

---
+++ include/binlog/advanced_log_macros.hpp
@@ -0,0 +1,103 @@
+#ifndef BINLOG_ADVANCED_LOG_MACROS_HPP
+#define BINLOG_ADVANCED_LOG_MACROS_HPP
+
+#include <binlog/Severity.hpp>
+#include <binlog/create_source_and_event_if.hpp>
+
+#include <chrono>
+
+/**
+ * BINLOG_<SEVERITY>_WC(writer, category, format, args...)
+ *
+ * Add a new event to `writer` if <SEVERITY> is greater-or-equal
+ * the minimum severity configured for `writer`,
+ * using the specified `category`.
+ *
+ * <SEVERITY> is one of: TRACE,DEBUG,INFO,WARN,ERROR,CRITICAL.
+ *
+ * The number of arguments must match the number of {} placeholders in `format`.
+ * The event is timestamped using std::chrono::system_clock.
+ * If <SEVERITY> is below the minimum severity configured
+ * for `writer`, no event is created, the arguments are not evaluated.
+ *
+ * @param writer binlog::SessionWriter
+ * @param category arbitrary valid symbol name
+ * @param format string literal with {} placeholders
+ * @param args... any number of serializable, tagged, log arguments. Can be empty
+ */
+
+#define BINLOG_TRACE_WC(writer, category, ...)                                  \
+  BINLOG_CREATE_SOURCE_AND_EVENT_IF(                                            \
+    writer, binlog::Severity::trace, category,                                  \
+    std::uint64_t(std::chrono::system_clock::now().time_since_epoch().count()), \
+    __VA_ARGS__                                                                 \
+  )                                                                             \
+  /**/
+
+#define BINLOG_DEBUG_WC(writer, category, ...)                                  \
+  BINLOG_CREATE_SOURCE_AND_EVENT_IF(                                            \
+    writer, binlog::Severity::debug, category,                                  \
+    std::uint64_t(std::chrono::system_clock::now().time_since_epoch().count()), \
+    __VA_ARGS__                                                                 \
+  )                                                                             \
+  /**/
+
+#define BINLOG_INFO_WC(writer, category, ...)                                   \
+  BINLOG_CREATE_SOURCE_AND_EVENT_IF(                                            \
+    writer, binlog::Severity::info, category,                                   \
+    std::uint64_t(std::chrono::system_clock::now().time_since_epoch().count()), \
+    __VA_ARGS__                                                                 \
+  )                                                                             \
+  /**/
+
+#define BINLOG_WARN_WC(writer, category, ...)                                   \
+  BINLOG_CREATE_SOURCE_AND_EVENT_IF(                                            \
+    writer, binlog::Severity::warning, category,                                \
+    std::uint64_t(std::chrono::system_clock::now().time_since_epoch().count()), \
+    __VA_ARGS__                                                                 \
+  )                                                                             \
+  /**/
+
+#define BINLOG_ERROR_WC(writer, category, ...)                                  \
+  BINLOG_CREATE_SOURCE_AND_EVENT_IF(                                            \
+    writer, binlog::Severity::error, category,                                  \
+    std::uint64_t(std::chrono::system_clock::now().time_since_epoch().count()), \
+    __VA_ARGS__                                                                 \
+  )                                                                             \
+  /**/
+
+#define BINLOG_CRITICAL_WC(writer, category, ...)                               \
+  BINLOG_CREATE_SOURCE_AND_EVENT_IF(                                            \
+    writer, binlog::Severity::critical, category,                               \
+    std::uint64_t(std::chrono::system_clock::now().time_since_epoch().count()), \
+    __VA_ARGS__                                                                 \
+  )                                                                             \
+  /**/
+
+/**
+ * BINLOG_<SEVERITY>_W(writer, format, args...)
+ *
+ * Add a new event to `writer` if <SEVERITY> is greater-or-equal
+ * the minimum severity configured for `writer`.
+ *
+ * <SEVERITY> is one of: TRACE,DEBUG,INFO,WARN,ERROR,CRITICAL.
+ *
+ * The number of arguments must match the number of {} placeholders in `format`.
+ * The event is timestamped using std::chrono::system_clock.
+ * If <SEVERITY> is below the minimum severity configured
+ * for `writer`, no event is created, the arguments are not evaluated.
+ * The event category will be "main".
+ *
+ * @param writer binlog::SessionWriter
+ * @param format string literal with {} placeholders
+ * @param args... any number of serializable, tagged, log arguments. Can be empty
+ */
+
+#define BINLOG_TRACE_W(   writer, ...) BINLOG_TRACE_WC(   writer, main, __VA_ARGS__)
+#define BINLOG_DEBUG_W(   writer, ...) BINLOG_DEBUG_WC(   writer, main, __VA_ARGS__)
+#define BINLOG_INFO_W(    writer, ...) BINLOG_INFO_WC(    writer, main, __VA_ARGS__)
+#define BINLOG_WARN_W(    writer, ...) BINLOG_WARN_WC(    writer, main, __VA_ARGS__)
+#define BINLOG_ERROR_W(   writer, ...) BINLOG_ERROR_WC(   writer, main, __VA_ARGS__)
+#define BINLOG_CRITICAL_W(writer, ...) BINLOG_CRITICAL_WC(writer, main, __VA_ARGS__)
+
+#endif // BINLOG_ADVANCED_LOG_MACROS_HPP
---
+++ test/unit/binlog/TestAdvancedLogMacros.cpp
@@ -0,0 +1,117 @@
+#include <binlog/advanced_log_macros.hpp>
+
+#include "test_utils.hpp"
+
+#include <binlog/Session.hpp>
+#include <binlog/SessionWriter.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+#include <map>
+#include <memory> // unique_ptr
+#include <string>
+#include <vector>
+
+BOOST_AUTO_TEST_SUITE(AdvancedLogMacros)
+
+BOOST_AUTO_TEST_CASE(wc_no_arg)
+{
+  binlog::Session session;
+  binlog::SessionWriter writer(session, 512);
+
+  BINLOG_TRACE_WC(   writer, my_cat, "Hello");
+  BINLOG_DEBUG_WC(   writer, my_cat, "Hello");
+  BINLOG_INFO_WC(    writer, my_cat, "Hello");
+  BINLOG_WARN_WC(    writer, my_cat, "Hello");
+  BINLOG_ERROR_WC(   writer, my_cat, "Hello");
+  BINLOG_CRITICAL_WC(writer, my_cat, "Hello");
+
+  const std::vector<std::string> expectedEvents{
+    "TRAC my_cat Hello",
+    "DEBG my_cat Hello",
+    "INFO my_cat Hello",
+    "WARN my_cat Hello",
+    "ERRO my_cat Hello",
+    "CRIT my_cat Hello",
+  };
+  BOOST_TEST(getEvents(session, "%S %C %m") == expectedEvents, boost::test_tools::per_element());
+}
+
+BOOST_AUTO_TEST_CASE(wc_more_args)
+{
+  binlog::Session session;
+  binlog::SessionWriter writer(session, 512);
+
+  const std::unique_ptr<int> pi(new int(123));
+  const std::unique_ptr<int> pn;
+  const std::map<int, int> m{{1,2}, {3,4}};
+
+  BINLOG_TRACE_WC(   writer, my_cat, "Hello {}", std::string("World"));
+  BINLOG_DEBUG_WC(   writer, my_cat, "Hello {}", std::vector<int>{1,2,3});
+  BINLOG_INFO_WC(    writer, my_cat, "Hello {} {}", pi, m);
+  BINLOG_WARN_WC(    writer, my_cat, "Hello {} {}", pn, pi);
+  BINLOG_ERROR_WC(   writer, my_cat, "Hello {}", m);
+  BINLOG_CRITICAL_WC(writer, my_cat, "Hello {} {} {} {}", pi, pn, m, 456);
+
+  const std::vector<std::string> expectedEvents{
+    "TRAC my_cat Hello World",
+    "DEBG my_cat Hello [1, 2, 3]",
+    "INFO my_cat Hello 123 [(1, 2), (3, 4)]",
+    "WARN my_cat Hello {null} 123",
+    "ERRO my_cat Hello [(1, 2), (3, 4)]",
+    "CRIT my_cat Hello 123 {null} [(1, 2), (3, 4)] 456",
+  };
+  BOOST_TEST(getEvents(session, "%S %C %m") == expectedEvents, boost::test_tools::per_element());
+}
+
+BOOST_AUTO_TEST_CASE(wc_actor_name)
+{
+  binlog::Session session;
+  binlog::SessionWriter writer(session, 512);
+
+  writer.setName("W");
+
+  BINLOG_TRACE_WC(   writer, my_cat, "Hello");
+  BINLOG_DEBUG_WC(   writer, my_cat, "Hello");
+  BINLOG_INFO_WC(    writer, my_cat, "Hello");
+  BINLOG_WARN_WC(    writer, my_cat, "Hello");
+  BINLOG_ERROR_WC(   writer, my_cat, "Hello");
+  BINLOG_CRITICAL_WC(writer, my_cat, "Hello");
+
+  const std::vector<std::string> expectedEvents{
+    "TRAC my_cat W Hello",
+    "DEBG my_cat W Hello",
+    "INFO my_cat W Hello",
+    "WARN my_cat W Hello",
+    "ERRO my_cat W Hello",
+    "CRIT my_cat W Hello",
+  };
+  BOOST_TEST(getEvents(session, "%S %C %n %m") == expectedEvents, boost::test_tools::per_element());
+}
+
+BOOST_AUTO_TEST_CASE(w_mixed)
+{
+  binlog::Session session;
+  binlog::SessionWriter writer(session, 512);
+
+  writer.setName("W2");
+
+  BINLOG_TRACE_W(   writer, "Hello");
+  BINLOG_DEBUG_W(   writer, "Hello");
+  BINLOG_INFO_W(    writer, "Hello {}", std::string("World"));
+  BINLOG_WARN_W(    writer, "Hello");
+  BINLOG_ERROR_W(   writer, "Hello");
+  BINLOG_CRITICAL_W(writer, "Hello {}", 123);
+
+  const std::vector<std::string> expectedEvents{
+    "TRAC main W2 Hello",
+    "DEBG main W2 Hello",
+    "INFO main W2 Hello World",
+    "WARN main W2 Hello",
+    "ERRO main W2 Hello",
+    "CRIT main W2 Hello 123",
+  };
+  BOOST_TEST(getEvents(session, "%S %C %n %m") == expectedEvents, boost::test_tools::per_element());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
