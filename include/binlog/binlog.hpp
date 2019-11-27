3bb3992cb27170a593afba5bd755ead60b57fe77
Benedek Thaler <Benedek.Thaler@morganstanley.com>
2019-11-27 16:02:36 +0100

    Add binlog.hpp, and a simple test program using it

---
+++ include/binlog/binlog.hpp
@@ -0,0 +1,13 @@
+#ifndef BINLOG_BINLOG_HPP
+#define BINLOG_BINLOG_HPP
+
+/*
+ * This is a convenience header
+ * which includes commonly used headers.
+ */
+
+#include <binlog/ArrayView.hpp>
+#include <binlog/basic_log_macros.hpp>
+#include <binlog/const_char_ptr_is_string.hpp>
+
+#endif // BINLOG_BINLOG_HPP
---
+++ test/integration/Logging.cpp
@@ -0,0 +1,25 @@
+#include <binlog/binlog.hpp>
+
+#include <iostream>
+
+int main()
+{
+  BINLOG_TRACE("Hello");
+  // Outputs: TRAC Hello
+  BINLOG_DEBUG("Hello");
+  // Outputs: DEBG Hello
+  BINLOG_INFO("Hello");
+  // Outputs: INFO Hello
+  BINLOG_WARN("Hello");
+  // Outputs: WARN Hello
+  BINLOG_ERROR("Hello");
+  // Outputs: ERRO Hello
+  BINLOG_CRITICAL("Hello");
+  // Outputs: CRIT Hello
+
+  BINLOG_INFO("Result: {}", 42);
+  // Outputs: INFO Result: 42
+
+  binlog::consume(std::cout);
+  return 0;
+}
