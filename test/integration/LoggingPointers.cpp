6dc4d28526a213b1fff637f1ec9c592a83311c0b
Benedek Thaler <Benedek.Thaler@morganstanley.com>
2019-11-28 12:29:43 +0100

    Add LoggingPointers integration test
    .clang-tidy: Do not force a closing namespace comment
    for short namespaces - clutters the documentation.

---
+++ test/integration/LoggingPointers.cpp
@@ -0,0 +1,64 @@
+#include <binlog/binlog.hpp>
+
+#include <boost/optional/optional.hpp>
+#include <boost/smart_ptr/scoped_ptr.hpp>
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <boost/smart_ptr/weak_ptr.hpp>
+
+#include <iostream>
+
+#include <memory>
+
+namespace mserialize { namespace detail {
+  template <typename T> struct is_optional<boost::optional<T>> : std::true_type {};
+}}
+
+int main()
+{
+  // Standard pointers
+
+  int* ptr = nullptr;
+  std::unique_ptr<int> uptr(std::make_unique<int>(1));
+  std::shared_ptr<int> sptr(std::make_shared<int>(2));
+  BINLOG_INFO("Pointers: {} {} {}", ptr, uptr, sptr);
+  // Outputs: Pointers: {null} 1 2
+
+  int value = 3;
+  ptr = &value;
+  uptr.reset();
+  sptr.reset();
+  BINLOG_INFO("Pointers: {} {} {}", ptr, uptr, sptr);
+  // Outputs: Pointers: 3 {null} {null}
+
+  // weak_ptr is not loggable by design, must be .lock()-ed first
+  static_assert(
+    !mserialize::detail::is_serializable<std::weak_ptr<int>>::value,
+    "std::weak_ptr is not loggable"
+  );
+
+  // Boost pointers - no specific adoption is required
+
+  boost::scoped_ptr<int> bscoped(new int(1));
+  boost::shared_ptr<int> bshared(new int(2));
+  BINLOG_INFO("Boost pointers: {} {}", bscoped, bshared);
+  // Outputs: Boost pointers: 1 2
+
+  bshared.reset();
+  BINLOG_INFO("Empty Boost pointers: {} {}", boost::scoped_ptr<bool>{}, bshared);
+  // Outputs: Empty Boost pointers: {null} {null}
+
+  static_assert(
+    !mserialize::detail::is_serializable<boost::weak_ptr<int>>::value,
+    "boost::weak_ptr is not loggable"
+  );
+
+  // Boost optional - loggable with the is_optional specialization above
+
+  boost::optional<int> opt(123);
+  boost::optional<int> emptyOpt;
+  BINLOG_INFO("Boost optionals: {} {}", opt, emptyOpt);
+  // Outputs: Boost optionals: 123 {null}
+
+  binlog::consume(std::cout);
+  return 0;
+}
