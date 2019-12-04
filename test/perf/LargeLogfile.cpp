9ab2b1bcf63f954376c2ad8f2a66cf5232c927d9
Benedek Thaler <Benedek.Thaler@morganstanley.com>
2019-12-04 16:52:40 +0100

    Add LargeLogfile perftest program
    This generates a ~300 MB logfile which
    can be used to measure the performance of bread.

---
+++ test/perf/LargeLogfile.cpp
@@ -0,0 +1,72 @@
+// Generate a large logfile, to measure bread performance
+
+#include <binlog/binlog.hpp>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+  BINLOG_INFO("Hello Large Logfile");
+
+  std::size_t size = 0;
+  std::size_t targetSize = 300 * 1024 * 1024;
+
+  const std::vector<int> numbers{1,2,3,4,5,6,7,8};
+
+  while (size < targetSize)
+  {
+    // duplicate code, to artificially increase the number of event sources
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("int {} bool {} char {}", 123, true, 'X');
+    BINLOG_DEBUG("Hello {}", "string");
+    BINLOG_DEBUG("More strings {} {} abc {}", "aaaaaaaaaaa", "bb", "ccccccccccccccccccccccccc");
+
+    BINLOG_INFO("Look, numbers: {}", numbers);
+
+    const auto result = binlog::consume(std::cout);
+    size += result.bytesConsumed;
+  }
+
+  return 0;
+}
