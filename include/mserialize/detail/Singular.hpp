1d8a6b44a923571bd8ba72b72d317fcee3544dfb
erenon <erenon2@gmail.com>
2020-07-27 14:11:58 +0200

    Add singular type tag predicate
    A tag of type T is singular, if objects of type T
    have only a single valid value.
    
    Objects of singular types are always serialized
    using 0 bytes.

---
+++ include/mserialize/detail/Singular.hpp
@@ -0,0 +1,71 @@
+#ifndef MSERIALIZE_DETAIL_SINGULAR_HPP
+#define MSERIALIZE_DETAIL_SINGULAR_HPP
+
+#include <mserialize/detail/tag_util.hpp>
+
+namespace mserialize {
+namespace detail {
+
+inline bool singular_impl(string_view, string_view, int);
+
+inline bool singular_tuple(string_view full_tag, string_view tag, int max_recursion)
+{
+  tag.remove_prefix(1); // drop (
+  tag.remove_suffix(1); // drop )
+  for (string_view elem_tag = tag_pop(tag); ! elem_tag.empty(); elem_tag = tag_pop(tag))
+  {
+    if (! singular_impl(full_tag, elem_tag, max_recursion)) { return false; }
+  }
+  return true;
+}
+
+inline bool singular_struct(string_view full_tag, string_view tag, int max_recursion)
+{
+  tag.remove_suffix(1); // drop }
+
+  string_view intro = remove_prefix_before(tag, '`');
+
+  if (tag.empty())
+  {
+    // perhaps a recursive struct?
+    const std::size_t intro_pos = full_tag.find(intro);
+    tag = string_view(full_tag.data() + intro_pos, full_tag.size() - intro_pos);
+    tag = tag_pop(tag);
+    tag.remove_prefix(intro.size());
+    tag.remove_suffix(1);
+    // if tag is not empty at this point, this is a recursive struct: non-singular
+    return tag.empty();
+  }
+
+  while (! tag.empty())
+  {
+    tag_pop_label(tag); // field_name
+    const string_view field_tag = tag_pop(tag);
+
+    if (! singular_impl(full_tag, field_tag, max_recursion)) { return false; }
+  }
+  return true;
+
+}
+
+inline bool singular_impl(string_view full_tag, string_view tag, int max_recursion)
+{
+  if (max_recursion == 0) { throw std::runtime_error("Recursion limit exceeded while visiting tag: " + full_tag.to_string()); }
+
+  if (tag.empty()) { return true; }
+
+  switch (tag.front())
+  {
+  case '(':
+    return singular_tuple(full_tag, tag, max_recursion - 1);
+  case '{':
+    return singular_struct(full_tag, tag, max_recursion - 1);
+  }
+
+  return false;
+}
+
+} // namespace detail
+} // namespace mserialize
+
+#endif // MSERIALIZE_DETAIL_SINGULAR_HPP
---
+++ test/unit/mserialize/singular.cpp
@@ -0,0 +1,70 @@
+#include <mserialize/singular.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+namespace {
+
+bool non_recursive_singular(mserialize::string_view tag)
+{
+  return mserialize::singular(tag, tag);
+}
+
+} // namespace
+
+BOOST_AUTO_TEST_SUITE(MserializeSingular)
+
+BOOST_AUTO_TEST_CASE(singular)
+{
+  BOOST_TEST(non_recursive_singular("()") == true);
+  BOOST_TEST(non_recursive_singular("(())") == true);
+  BOOST_TEST(non_recursive_singular("((()))") == true);
+
+  BOOST_TEST(non_recursive_singular("{Empty}") == true);
+  BOOST_TEST(non_recursive_singular("{Empty`x'()}") == true);
+  BOOST_TEST(non_recursive_singular("{Empty`x'(())}") == true);
+  BOOST_TEST(non_recursive_singular("{Empty`x'{Nil}}") == true);
+  BOOST_TEST(non_recursive_singular("{Empty`x'{Nil}`y'{Nul}}") == true);
+
+}
+
+BOOST_AUTO_TEST_CASE(not_singular)
+{
+  BOOST_TEST(non_recursive_singular("y") == false);
+  BOOST_TEST(non_recursive_singular("c") == false);
+
+  BOOST_TEST(non_recursive_singular("b") == false);
+  BOOST_TEST(non_recursive_singular("s") == false);
+  BOOST_TEST(non_recursive_singular("i") == false);
+  BOOST_TEST(non_recursive_singular("l") == false);
+
+  BOOST_TEST(non_recursive_singular("B") == false);
+  BOOST_TEST(non_recursive_singular("S") == false);
+  BOOST_TEST(non_recursive_singular("I") == false);
+  BOOST_TEST(non_recursive_singular("L") == false);
+
+  BOOST_TEST(non_recursive_singular("f") == false);
+  BOOST_TEST(non_recursive_singular("d") == false);
+  BOOST_TEST(non_recursive_singular("D") == false);
+
+  BOOST_TEST(non_recursive_singular("[i") == false);
+  BOOST_TEST(non_recursive_singular("[f") == false);
+  BOOST_TEST(non_recursive_singular("[y") == false);
+
+  BOOST_TEST(non_recursive_singular("(i)") == false);
+
+  BOOST_TEST(non_recursive_singular("<>") == false);
+  BOOST_TEST(non_recursive_singular("<i>") == false);
+  BOOST_TEST(non_recursive_singular("<()>") == false);
+
+  BOOST_TEST(non_recursive_singular("/i`E'\\") == false);
+
+  BOOST_TEST(non_recursive_singular("{List`n'<0List>}") == false);
+}
+
+BOOST_AUTO_TEST_CASE(recursive)
+{
+  BOOST_TEST(mserialize::singular("{Empty`x'{Empty}}", "{Empty}") == false); // infinite recursion
+  BOOST_TEST(mserialize::singular("{R,`r'[{R}}", "{R}") == false);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
